d72a8374f178a3a62d8545241486c40857559646
Michael Schano <michael.schano@gmx.at>
2020-02-14 15:43:29 +0100

    braucht noch feinschliff

---
+++ myscript.js
@@ -0,0 +1,5 @@
+$(".like-button").on("click", function(e) {
+    var $counter = $(this).find(".count");
+    var count = $counter.text() | 0; //corose current count to an int
+    $counter.text(count + 1); //set new count
+});
